7a9ad2af8ff1b60fac724eb97fddcd53df884d35
Powmer <punchabunch3000@gmail.com>
2025-06-12 23:48:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,27 @@ function toggleSidebar() {
     content.classList.add('blurred');
   }
 }
+
+  const fadeBg = document.getElementById("fadeBackground");
+  const images = [
+    "topimage(1).jpeg",
+    "topimage(2).jpeg",
+    "topimage(3).jpeg",
+    "topimage(4).jpeg"
+  ];
+
+  let current = 0;
+
+  function updateBackground() {
+    fadeBg.style.opacity = 0;
+    setTimeout(() => {
+      fadeBg.style.backgroundImage = `url(${images[current]})`;
+      fadeBg.style.opacity = 0.4;
+      current = (current + 1) % images.length;
+    }, 1000); // tempo do fade
+  }
+
+
+  fadeBg.style.backgroundImage = `url(${images[0]})`;
+  setInterval(updateBackground, 5000); 
+
